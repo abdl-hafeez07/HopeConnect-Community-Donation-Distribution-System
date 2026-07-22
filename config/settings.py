c5c6a1570be808e9f8adf7f4fb1c5c6782376e71
unknown <abdulhafeez0906@gmail.com>
2026-07-22 22:59:40 +0530

    Phase 4: Add UserProfile model and database migrations

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project Apps
+    'core',
+    'accounts',
+    'donations',
+    'donation_requests',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
