875fbe6b9bcaf8821f4b803fe22dbde02ca908f2
unknown <abdulhafeez0906@gmail.com>
2026-07-25 23:51:07 +0530

    Improve UserProfile admin management

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'role',
+        'phone',
+        'is_verified',
+        'created_at',
+    )
+
+    list_filter = (
+        'role',
+        'is_verified',
+    )
+
+    search_fields = (
+        'user__username',
+        'user__email',
+        'phone',
+    )
